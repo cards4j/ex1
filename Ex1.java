69e73ae45ec12c56a4a46af27896c0578ba8dcb9
John Meyer <john.meyer@app-project.com>
2022-01-26 16:34:08 -0800

    EFJ-188 Set up Ex1 in Own Repo

---
+++ Ex1.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * Java datatypes:  invalid  literal.
+ */
+
+public class Ex1  {
+
+    public static void main(String[] args) {
+        Integer a = Integer.valueOf(7);
+        int c = a * 5.0;
+        System.out.println(c);
+    }
+}
