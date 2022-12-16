922a46de87a6d3ce1367dd85c50b7ecb8aa23128
EC2 Default User <ec2-user@ip-172-31-3-22.us-west-2.compute.internal>
2022-12-16 16:41:41 +0000

    Add a weird integer type

--- Ex1.java
+++ Ex1.java
@@ -3,11 +3,13 @@ public class Ex1  {
         int a = Integer.valueOf("1", 16);
         int b = Integer.parseInt("-FFFF", 16);
         int c = Integer.parseInt("FFFF", 16);
+	int d = Integer.praseInt("ABCD", 32);
 
         String format = "value: %+,12d   toUnsignedString: %10s   %n";
         System.out.format(format, a, a.toUnsignedString());
         System.out.format(format, b, b.toUnsignedString());
         System.out.format(format, c, c.toUnsignedString());
+        System.out.format(format, d, d.toUnsignedString());
     }
 }
 
